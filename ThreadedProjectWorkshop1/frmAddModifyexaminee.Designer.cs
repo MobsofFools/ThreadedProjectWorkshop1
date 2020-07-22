ad8439986acd6b660c42d27e0c29ab720cc56de4
MobsofFools <32505517+MobsofFools@users.noreply.github.com>
2020-07-22 17:41:24 -0600

    init

---
+++ ThreadedProjectWorkshop1/frmAddModifyexaminee.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace ThreadedProjectWorkshop1
+{
+    partial class frmAddModifyexaminee
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Label examineeEmailLabel;
+            System.Windows.Forms.Label examineeFirstnameLabel;
+            System.Windows.Forms.Label examineeIDLabel;
+            System.Windows.Forms.Label examineeLastnameLabel;
+            this.examineeBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.examineeEmailTextBox = new System.Windows.Forms.TextBox();
+            this.examineeFirstnameTextBox = new System.Windows.Forms.TextBox();
+            this.examineeIDTextBox = new System.Windows.Forms.TextBox();
+            this.examineeLastnameTextBox = new System.Windows.Forms.TextBox();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            examineeEmailLabel = new System.Windows.Forms.Label();
+            examineeFirstnameLabel = new System.Windows.Forms.Label();
+            examineeIDLabel = new System.Windows.Forms.Label();
+            examineeLastnameLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.examineeBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // examineeBindingSource
+            // 
+            this.examineeBindingSource.DataSource = typeof(ThreadedProjectWorkshop1.Examinee);
+            // 
+            // examineeEmailLabel
+            // 
+            examineeEmailLabel.AutoSize = true;
+            examineeEmailLabel.Location = new System.Drawing.Point(36, 147);
+            examineeEmailLabel.Name = "examineeEmailLabel";
+            examineeEmailLabel.Size = new System.Drawing.Size(111, 17);
+            examineeEmailLabel.TabIndex = 1;
+            examineeEmailLabel.Text = "Examinee Email:";
+            // 
+            // examineeEmailTextBox
+            // 
+            this.examineeEmailTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.examineeBindingSource, "ExamineeEmail", true));
+            this.examineeEmailTextBox.Location = new System.Drawing.Point(181, 144);
+            this.examineeEmailTextBox.Name = "examineeEmailTextBox";
+            this.examineeEmailTextBox.Size = new System.Drawing.Size(220, 22);
+            this.examineeEmailTextBox.TabIndex = 2;
+            // 
+            // examineeFirstnameLabel
+            // 
+            examineeFirstnameLabel.AutoSize = true;
+            examineeFirstnameLabel.Location = new System.Drawing.Point(36, 68);
+            examineeFirstnameLabel.Name = "examineeFirstnameLabel";
+            examineeFirstnameLabel.Size = new System.Drawing.Size(139, 17);
+            examineeFirstnameLabel.TabIndex = 3;
+            examineeFirstnameLabel.Text = "Examinee Firstname:";
+            // 
+            // examineeFirstnameTextBox
+            // 
+            this.examineeFirstnameTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.examineeBindingSource, "ExamineeFirstname", true));
+            this.examineeFirstnameTextBox.Location = new System.Drawing.Point(181, 65);
+            this.examineeFirstnameTextBox.Name = "examineeFirstnameTextBox";
+            this.examineeFirstnameTextBox.Size = new System.Drawing.Size(220, 22);
+            this.examineeFirstnameTextBox.TabIndex = 4;
+            // 
+            // examineeIDLabel
+            // 
+            examineeIDLabel.AutoSize = true;
+            examineeIDLabel.Location = new System.Drawing.Point(36, 31);
+            examineeIDLabel.Name = "examineeIDLabel";
+            examineeIDLabel.Size = new System.Drawing.Size(90, 17);
+            examineeIDLabel.TabIndex = 5;
+            examineeIDLabel.Text = "Examinee ID:";
+            // 
+            // examineeIDTextBox
+            // 
+            this.examineeIDTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.examineeBindingSource, "ExamineeID", true));
+            this.examineeIDTextBox.Location = new System.Drawing.Point(181, 28);
+            this.examineeIDTextBox.Name = "examineeIDTextBox";
+            this.examineeIDTextBox.Size = new System.Drawing.Size(220, 22);
+            this.examineeIDTextBox.TabIndex = 6;
+            // 
+            // examineeLastnameLabel
+            // 
+            examineeLastnameLabel.AutoSize = true;
+            examineeLastnameLabel.Location = new System.Drawing.Point(36, 106);
+            examineeLastnameLabel.Name = "examineeLastnameLabel";
+            examineeLastnameLabel.Size = new System.Drawing.Size(139, 17);
+            examineeLastnameLabel.TabIndex = 7;
+            examineeLastnameLabel.Text = "Examinee Lastname:";
+            // 
+            // examineeLastnameTextBox
+            // 
+            this.examineeLastnameTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.examineeBindingSource, "ExamineeLastname", true));
+            this.examineeLastnameTextBox.Location = new System.Drawing.Point(181, 103);
+            this.examineeLastnameTextBox.Name = "examineeLastnameTextBox";
+            this.examineeLastnameTextBox.Size = new System.Drawing.Size(220, 22);
+            this.examineeLastnameTextBox.TabIndex = 8;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(254, 208);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 12;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(125, 208);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 11;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // frmAddModifyexaminee
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(474, 279);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(examineeEmailLabel);
+            this.Controls.Add(this.examineeEmailTextBox);
+            this.Controls.Add(examineeFirstnameLabel);
+            this.Controls.Add(this.examineeFirstnameTextBox);
+            this.Controls.Add(examineeIDLabel);
+            this.Controls.Add(this.examineeIDTextBox);
+            this.Controls.Add(examineeLastnameLabel);
+            this.Controls.Add(this.examineeLastnameTextBox);
+            this.Name = "frmAddModifyexaminee";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.frmAddModifyexaminee_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.examineeBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.BindingSource examineeBindingSource;
+        private System.Windows.Forms.TextBox examineeEmailTextBox;
+        private System.Windows.Forms.TextBox examineeFirstnameTextBox;
+        private System.Windows.Forms.TextBox examineeIDTextBox;
+        private System.Windows.Forms.TextBox examineeLastnameTextBox;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
---
+++ ThreadedProjectWorkshop1/frmSearch.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace ThreadedProjectWorkshop1
+{
+    partial class frmSearch
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSearchBy = new System.Windows.Forms.Label();
+            this.cbSearchBy = new System.Windows.Forms.ComboBox();
+            this.btnSearchBy = new System.Windows.Forms.Button();
+            this.examineeDataGridView = new System.Windows.Forms.DataGridView();
+            this.searchDataGridView = new System.Windows.Forms.DataGridView();
+            this.btnDeleterecord = new System.Windows.Forms.Button();
+            this.btnModify = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.txtSearchBy = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.examineeDataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.searchDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblSearchBy
+            // 
+            this.lblSearchBy.AutoSize = true;
+            this.lblSearchBy.Location = new System.Drawing.Point(990, 23);
+            this.lblSearchBy.Name = "lblSearchBy";
+            this.lblSearchBy.Size = new System.Drawing.Size(77, 17);
+            this.lblSearchBy.TabIndex = 0;
+            this.lblSearchBy.Text = "Search By:";
+            // 
+            // cbSearchBy
+            // 
+            this.cbSearchBy.FormattingEnabled = true;
+            this.cbSearchBy.Items.AddRange(new object[] {
+            "Examinee ID",
+            "Exam ID",
+            "Session ID"});
+            this.cbSearchBy.Location = new System.Drawing.Point(1073, 23);
+            this.cbSearchBy.Name = "cbSearchBy";
+            this.cbSearchBy.Size = new System.Drawing.Size(121, 24);
+            this.cbSearchBy.TabIndex = 1;
+            // 
+            // btnSearchBy
+            // 
+            this.btnSearchBy.Location = new System.Drawing.Point(1345, 23);
+            this.btnSearchBy.Name = "btnSearchBy";
+            this.btnSearchBy.Size = new System.Drawing.Size(75, 23);
+            this.btnSearchBy.TabIndex = 2;
+            this.btnSearchBy.Text = "Search";
+            this.btnSearchBy.UseVisualStyleBackColor = true;
+            this.btnSearchBy.Click += new System.EventHandler(this.btnSearchBy_Click);
+            // 
+            // examineeDataGridView
+            // 
+            this.examineeDataGridView.AllowUserToResizeRows = false;
+            this.examineeDataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.examineeDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.examineeDataGridView.Location = new System.Drawing.Point(37, 73);
+            this.examineeDataGridView.Name = "examineeDataGridView";
+            this.examineeDataGridView.RowHeadersWidth = 51;
+            this.examineeDataGridView.RowTemplate.Height = 24;
+            this.examineeDataGridView.Size = new System.Drawing.Size(519, 584);
+            this.examineeDataGridView.TabIndex = 3;
+            this.examineeDataGridView.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.examineeDataGridView_CellClick);
+            // 
+            // searchDataGridView
+            // 
+            this.searchDataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.searchDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.searchDataGridView.Location = new System.Drawing.Point(583, 73);
+            this.searchDataGridView.Name = "searchDataGridView";
+            this.searchDataGridView.RowHeadersWidth = 51;
+            this.searchDataGridView.RowTemplate.Height = 24;
+            this.searchDataGridView.Size = new System.Drawing.Size(837, 584);
+            this.searchDataGridView.TabIndex = 4;
+            // 
+            // btnDeleterecord
+            // 
+            this.btnDeleterecord.Location = new System.Drawing.Point(258, 682);
+            this.btnDeleterecord.Name = "btnDeleterecord";
+            this.btnDeleterecord.Size = new System.Drawing.Size(75, 23);
+            this.btnDeleterecord.TabIndex = 13;
+            this.btnDeleterecord.Text = "Delete Record";
+            this.btnDeleterecord.UseVisualStyleBackColor = true;
+            this.btnDeleterecord.Click += new System.EventHandler(this.btnDeleterecord_Click);
+            // 
+            // btnModify
+            // 
+            this.btnModify.Location = new System.Drawing.Point(144, 682);
+            this.btnModify.Name = "btnModify";
+            this.btnModify.Size = new System.Drawing.Size(108, 23);
+            this.btnModify.TabIndex = 12;
+            this.btnModify.Text = "Modify Record";
+            this.btnModify.UseVisualStyleBackColor = true;
+            this.btnModify.Click += new System.EventHandler(this.btnModify_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(37, 682);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(101, 23);
+            this.btnAdd.TabIndex = 11;
+            this.btnAdd.Text = "Add Record";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(1239, 682);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 15;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(1345, 682);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 14;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // txtSearchBy
+            // 
+            this.txtSearchBy.Location = new System.Drawing.Point(1211, 23);
+            this.txtSearchBy.Name = "txtSearchBy";
+            this.txtSearchBy.Size = new System.Drawing.Size(121, 22);
+            this.txtSearchBy.TabIndex = 16;
+            // 
+            // frmSearch
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1446, 735);
+            this.Controls.Add(this.txtSearchBy);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnDeleterecord);
+            this.Controls.Add(this.btnModify);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.searchDataGridView);
+            this.Controls.Add(this.examineeDataGridView);
+            this.Controls.Add(this.btnSearchBy);
+            this.Controls.Add(this.cbSearchBy);
+            this.Controls.Add(this.lblSearchBy);
+            this.Name = "frmSearch";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmSearch_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.examineeDataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.searchDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSearchBy;
+        private System.Windows.Forms.ComboBox cbSearchBy;
+        private System.Windows.Forms.Button btnSearchBy;
+        private System.Windows.Forms.DataGridView examineeDataGridView;
+        private System.Windows.Forms.DataGridView searchDataGridView;
+        private System.Windows.Forms.Button btnDeleterecord;
+        private System.Windows.Forms.Button btnModify;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox txtSearchBy;
+    }
+}
+
